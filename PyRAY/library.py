8300f855b874a8b94108aac58dd96ab0ddafa63d
Victoriya V. Forsythe <116663397+victoriyaforsythe@users.noreply.github.com>
2025-05-01 15:31:05 -0400

    Update library.py
    comments and heads

--- PyRAY/library.py
+++ PyRAY/library.py
@@ -2,6 +2,7 @@ import numpy as np
 import PyIRI
 import PyIRI.main_library as ml
 from scipy.interpolate import interp1d
+from PyRAY import logger
 
 
 def constants():
@@ -18,7 +19,9 @@ def constants():
     -----
     This function provides constants used in virtual height calculations.
     """
+    # Constant to convert density to frequency (MHz)
     cp = 8.97866275
+    # Proton gyrofrequency constant (Hz/T)
     g_p = 2.799249247e10
     return cp, g_p
 
@@ -36,6 +39,7 @@ def den2freq(density):
     frequency : float or array-like
         Plasma frequency in Hz.
     """
+    # Declaring constants
     cp, _ = constants()
     frequency = np.sqrt(density) * cp
     return frequency
@@ -54,8 +58,9 @@ def freq2den(frequency):
     density : float or array-like
         Plasma density in m^-3.
     """
+    # Declaring constants
     cp, _ = constants()
-    density = (frequency / cp) ** 2
+    density = (frequency / cp)**2
     return density
 
 
@@ -74,7 +79,7 @@ def find_X(n_e, f):
     X : float or array-like
         Ratio (f_N / f)^2.
     """
-    X = den2freq(n_e) ** 2 / f ** 2
+    X = (den2freq(n_e))**2 / f**(2)
     return X
 
 
@@ -119,35 +124,46 @@ def find_mu_mup(X, Y, bpsi, mode):
     mup : array-like
         Group refractive index μ′.
     """
+    # Compute transverse and longitudinal components of Y
     YT = Y * np.sin(np.deg2rad(bpsi))
     YL = Y * np.cos(np.deg2rad(bpsi))
+
+    # Compute 1-X
     Xm1 = 1.0 - X
 
-    alpha = 0.25 * YT ** 4 + YL ** 2 * Xm1 ** 2
+    # Calculate alpha and beta as intermediate terms for refractive index
+    alpha = 0.25 * YT**4 + YL**2 * Xm1**2
     beta = np.sqrt(alpha)
 
-    modeMult = 1.0 if mode == 'O' else -1.0
-    D = Xm1 - 0.5 * YT ** 2 + modeMult * beta
-    mu = np.sqrt(1.0 - X * Xm1 / D)
+    # Set mode multiplier depending on propagation mode
+    if mode == 'O':
+        modeMult = 1.
+    if mode == 'X':
+        modeMult = -1.
 
-    # Clamp values
-    mu[mu < 0.0] = 0.0
-    mu[mu > 1.0] = np.nan
+    # Appleton-Hartree denominator and mu
+    D = Xm1 - 0.5 * YT**2 + modeMult * beta
+    mu = np.sqrt(1. - X * Xm1 / D)
 
-    # Partial derivatives for group index
-    dbetadX = -YL ** 2 * Xm1 / beta
-    dDdX = -1.0 + modeMult * dbetadX
+    # Apply physical constraints on refractive index
+    mu[np.where(mu < 0.)] = 0.
+    mu[np.where(mu > 1.)] = np.nan
 
-    dalphadY = (YT ** 3 * np.sin(np.deg2rad(bpsi))) + (
-        2.0 * YL * Xm1 ** 2 * np.cos(np.deg2rad(bpsi))
-    )
+    # Derivatives with respect to X and Y
+    dbetadX = -YL**2 * Xm1 / beta
+    dDdX = -1. + modeMult * dbetadX
+
+    dalphadY = (YT**3 * np.sin(np.deg2rad(bpsi))) + (2. * YL * Xm1**2 * np.cos(np.deg2rad(bpsi)))
     dbetadY = 0.5 * dalphadY / beta
     dDdY = -YT * np.sin(np.deg2rad(bpsi)) + modeMult * dbetadY
 
-    dmudY = (X * Xm1 * dDdY) / (2.0 * mu * D ** 2)
-    dmudX = (1.0 / (2.0 * mu * D)) * (2.0 * X - 1.0 + X * Xm1 / D * dDdX)
+    # Compute partial derivatives of mu for corrected index
+    dmudY = (X * Xm1 * dDdY) / (2. * mu * D**2)
+    dmudX = (1. / (2. * mu * D)) * (2. * X - 1. + X * Xm1 / D * dDdX)
 
-    mup = mu - (2.0 * X * dmudX + Y * dmudY)
+    # Modified refractive index considering dispersion effects
+    mup = mu - (2. * X * dmudX + Y * dmudY)
+
     return mu, mup
 
 
@@ -174,7 +190,11 @@ def find_vh(X, Y, bpsi, dh, alt_min, mode):
     vh : array-like
         Virtual height in km.
     """
+    # Find the phase refractive index μ and the group refractive index μ′ for
+    # ordinary (O) and extraordinary (X) modes of signal propagation
     _, mup = find_mu_mup(X, Y, bpsi, mode)
+
+    # Find virtual height as vertical integral through μ′
     vh = np.nansum(mup * dh, axis=1) + alt_min
     return vh
 
@@ -195,15 +215,19 @@ def smooth_nonuniform_grid(start, end, n_points, sharpness):
     x : ndarray
         Non-uniformly spaced grid.
     """
+    # Uniform grid [0, 1]
     u = np.linspace(0.0, 1.0, n_points)
+
+    # Flip to make fine resolution near 'end'
     flipped_u = 1.0 - u
+
     factor = (np.exp(sharpness * flipped_u) - 1.0) / (np.exp(sharpness) - 1.0)
-    x = 1.0 - (start + (end - start) * factor)
+    x = 1. - (start + (end - start) * factor)
     return x
 
 
 def regrid_to_nonuniform_grid(f, n_e, b, bpsi, aalt, npoints):
-    """Regrid profile to smooth non-uniform vertical grid.
+"""Regrid profile to smooth non-uniform vertical grid.
 
     Parameters
     ----------
@@ -224,34 +248,52 @@ def regrid_to_nonuniform_grid(f, n_e, b, bpsi, aalt, npoints):
     -------
     Tuple of regridded arrays.
     """
-    start, end, sharpness = 0, 1, 10.0
+
+    # Create non-regular grid that has low resolution near zero and hight
+    # resolution near one
+    start = 0
+    end = 1
+    sharpness = 10.
     multiplier = smooth_nonuniform_grid(start, end, npoints, sharpness)
 
     N_grid = multiplier.size
     N_freq = f.size
-    ind_grid = np.arange(N_grid)
+    ind_grid = np.arange(0, N_grid, 1)
 
-    # Trim input to foF2
+    # Limit input arrays to the fof2 of the ionosphere
     ind_max = np.argmax(n_e)
-    n_e, b, bpsi, aalt = n_e[:ind_max], b[:ind_max], bpsi[:ind_max], aalt[:ind_max]
+    n_e = n_e[0: ind_max]
+    b = b[0: ind_max]
+    bpsi = bpsi[0: ind_max]
+    aalt = aalt[0: ind_max]
 
+    # How close to the reflection height do we want to get
     dh = 1e-20
+
+    # An array of critical height for the given ionosonde frequency
+    # We subtract dh so that the critical height is not exactly reached
     critical_height = np.interp(f, den2freq(n_e), aalt) - dh
 
+    # Make arrays 2-D
     multiplier_2d = np.full((N_freq, N_grid), multiplier)
     critical_height_2d = np.transpose(np.full((N_grid, N_freq), critical_height))
     new_alt_2d = multiplier_2d * (critical_height_2d - aalt[0]) + aalt[0]
 
     dh_2d = np.concatenate((np.diff(new_alt_2d, axis=1), np.full((N_freq, 1), dh)), axis=1)
-    new_ind_2d = np.full((N_freq, N_grid), ind_grid)
-    new_alt_1d = np.reshape(new_alt_2d, new_ind_2d.size)
 
+    new_ind_2d = np.full((N_freq, N_grid), ind_grid)
+
+    # Flattened array of new altitudes where we want to sample the density
+    # profile, so we can apply 1-D Numpy interpolation, that is faster
+    new_alt_1d = np.reshape(new_alt_2d, new_ind_2d.size)
+    # Create arrays on the modified grid
     den_mod = np.reshape(np.interp(new_alt_1d, aalt, n_e), new_alt_2d.shape)
     bmag_mod = np.reshape(np.interp(new_alt_1d, aalt, b), new_alt_2d.shape)
     bpsi_mod = np.reshape(np.interp(new_alt_1d, aalt, bpsi), new_alt_2d.shape)
     ionosonde_freq_mod = np.transpose(np.full((N_grid, N_freq), f))
 
     return ionosonde_freq_mod, den_mod, bmag_mod, bpsi_mod, dh_2d, critical_height_2d, new_ind_2d, ind_grid
+
 
 
 def vertical_to_magnetic_angle(inclination_deg):
@@ -267,7 +309,8 @@ def vertical_to_magnetic_angle(inclination_deg):
     vertical_angle : float or ndarray
         Angle between vertical and magnetic field in degrees.
     """
-    return 90.0 - np.abs(inclination_deg)
+    vertical_angle = 90.0 - np.abs(inclination_deg)
+    return vertical_angle
 
 
 def virtical_forward_operator(freq, den, bmag, bpsi, alt, mode='O', n_points=2000):
@@ -295,18 +338,45 @@ def virtical_forward_operator(freq, den, bmag, bpsi, alt, mode='O', n_points=200
     vh : ndarray
         Virtual height in km.
     """
+    # Limit the ionosonde frequency array up tp the ionospheric critical
+    # frequency foF2 and convert form MHz to Hz.
     foF2 = np.max(den2freq(den))
+
+    # Index where ionosonde frequency is less then foF2 value
     ind = np.where((freq * 1e6) < foF2)
+
+    # Select ionosonde frequency with this criteria
     freq_lim = freq[ind] * 1e6
-    vh = np.full(freq.shape, np.nan)
+    
+    # Make empty array to collect virtual height of the same size as input
+    # frequency array
+    vh = np.zeros((freq.size)) + np.nan
 
-    (freq_mod, den_mod, bmag_mod, bpsi_mod, dh_2d,
-     critical_height_2d, new_ind_2d, ind_grid) = regrid_to_nonuniform_grid(
-        freq_lim, den, bmag, bpsi, alt, n_points
-    )
+    # Interpolate input arrays into a new stretched grid based on the
+    # reflective height for each ionosonde frequency
+    # Frequency needs to be converted to MHz from Hz
+    (freq_mod,
+    den_mod,
+    bmag_mod,
+    bpsi_mod,
+    dh_2d,
+    critical_height_2d,
+    new_ind_2d,
+    ind_grid) = regrid_to_nonuniform_grid(freq_lim,
+                                          den,
+                                          bmag,
+                                          bpsi,
+                                          alt,
+                                          n_points)
 
+    # Find the ratio of the square of the plasma frequency f_N to the square of
+    # the ionosonde frequency f.
     aX = find_X(den_mod, freq_mod)
+
+    # Find the ratio of electron gyrofrequency and the ionosonde frequency
     aY = find_Y(freq_mod, bmag_mod)
+
+    # Find virtual height
     vh[ind] = find_vh(aX, aY, bpsi_mod, dh_2d, np.min(alt), mode)
 
     return vh
